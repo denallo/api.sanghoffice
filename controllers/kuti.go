ea393649e3608ced5e6f78e001c186c9c53150c6
Shizhe <denallo@163.com>
2019-11-30 19:50:53 +0800

    feat: 接口/kuties[get]新增type和range选项

--- controllers/kuti.go
+++ controllers/kuti.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"strconv"
 
+	"github.com/astaxie/beego/orm"
+
 	"api.sanghoffice/models"
 	"api.sanghoffice/tools"
 	"github.com/astaxie/beego"
@@ -29,11 +31,69 @@ func (ctrl *KutiController) PtrData() *map[interface{}]interface{} {
 }
 
 // @router / [get]
-func (ctrl *KutiController) Get() {
-	kutiForSex, _ := strconv.Atoi(ctrl.Ctx.Input.Param("sex"))
-	retJson := models.GetKuties(kutiForSex)
-	ctrl.Data["json"] = retJson
-	ctrl.ServeJSON()
+func (this *KutiController) Get() {
+	kutiForSex, _ := strconv.Atoi(this.Ctx.Input.Param("sex"))
+	_, getRange := this.GetInt("range")
+	_, getValidTypes := this.GetInt("types")
+	retJson := map[string]interface{}{}
+	o := orm.NewOrm()
+	if nil == getRange {
+		kutiType, _ := this.GetInt("type")
+		kuties := []*models.Kuti{}
+		cnt, err := o.QueryTable("tb_kuti").
+			Filter("for_sex", kutiForSex).
+			Filter("broken", 0).
+			Filter("type", kutiType).
+			OrderBy("number").
+			All(&kuties)
+		if nil != err {
+			ReplyError(this, STATUSCODE_EXCEPTIONOCCUR,
+				MESSAGE_EXCEPTIONOCCUR+err.Error())
+			return
+		} else if 0 == cnt {
+			ReplyError(this,
+				STATUSCODE_INVALIDREQUEST,
+				MESSAGE_INVALIDREQUEST+fmt.Sprintf(
+					"found no record of type %d", kutiType))
+			return
+		}
+		eliminatedKuties := []*models.Kuti{}
+		_, err = o.QueryTable("tb_kuti").
+			Filter("for_sex", kutiForSex).
+			Filter("broken", 1).
+			Filter("type", kutiType).
+			OrderBy("number").
+			All(&eliminatedKuties)
+		if nil != err {
+			ReplyError(this, STATUSCODE_EXCEPTIONOCCUR,
+				MESSAGE_EXCEPTIONOCCUR+err.Error())
+			return
+		}
+		minNumber := kuties[0].Number
+		maxNumber := kuties[len(kuties)-1].Number
+		eliminated := []int{}
+		for i := 0; i < len(eliminatedKuties); i++ {
+			eliminated = append(eliminated, eliminatedKuties[i].Number)
+		}
+		retJson["from"] = minNumber
+		retJson["to"] = maxNumber
+		retJson["eliminated"] = eliminated
+	} else if nil == getValidTypes {
+		validTypes := []int{}
+		_, err := o.Raw("SELECT DISTINCT type FROM tb_kuti "+
+			"WHERE for_sex=? AND broken=0 ORDER BY type",
+			kutiForSex).QueryRows(&validTypes)
+		if nil != err {
+			ReplyError(this, STATUSCODE_EXCEPTIONOCCUR,
+				MESSAGE_EXCEPTIONOCCUR+err.Error())
+			return
+		}
+		retJson["validTypes"] = validTypes
+	} else {
+		retJson = models.GetKuties(kutiForSex)
+	}
+	this.Data["json"] = retJson
+	this.ServeJSON()
 }
 
 // @router /status [patch]
