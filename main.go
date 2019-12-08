e044e3915c93a6f4c67585f4ca8ec0def1449ef8
Shizhe <denallo@163.com>
2019-12-08 21:42:26 +0800

    feat: main模块取消对RunMode的判断

--- main.go
+++ main.go
@@ -8,17 +8,27 @@ import (
 )
 
 func main() {
-	if beego.BConfig.RunMode == "dev" {
-		beego.BConfig.WebConfig.DirectoryIndex = true
-		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
-		beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-			AllowAllOrigins:  true,
-			AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
-			AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type", "SessionID"},
-			ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
-			AllowCredentials: true,
-		}))
-		beego.InsertFilter("*", beego.BeforeRouter, controllers.FilterSessionID)
-	}
+	beego.BConfig.WebConfig.DirectoryIndex = true
+	beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type", "SessionID"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		AllowCredentials: true,
+	}))
+	beego.InsertFilter("*", beego.BeforeRouter, controllers.FilterSessionID)
+	// if beego.BConfig.RunMode == "dev" {
+	// 	beego.BConfig.WebConfig.DirectoryIndex = true
+	// 	beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	// 	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+	// 		AllowAllOrigins:  true,
+	// 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+	// 		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type", "SessionID"},
+	// 		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+	// 		AllowCredentials: true,
+	// 	}))
+	// 	beego.InsertFilter("*", beego.BeforeRouter, controllers.FilterSessionID)
+	// }
 	beego.Run()
 }
